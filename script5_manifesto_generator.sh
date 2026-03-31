ca570c6d234af4edec443c093e3eef7e88a76e91
VKRAHUL444 <vk.rahul444@gmail.com>
2026-03-31 23:26:21 +0530

    Update author name in manifesto generator script

--- script5_manifesto_generator.sh
+++ script5_manifesto_generator.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 # Script 5: Open Source Manifesto Generator
-# Author: Rajkumar S (24BCE10447)
+# Author: Rahul Gandhi V (24MEI10161)
 
 # --- Alias (concept demonstration) ---
 shopt -s expand_aliases
